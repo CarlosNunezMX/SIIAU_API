8aff6b93edbde9c04c0770df6ff6a1959b92e6e8
Carlos N <carlitosnu41@gmail.com>
2023-06-11 00:29:08 -0600

    DUMMY

--- source/index.js
+++ source/index.js
@@ -1,4 +1,6 @@
 import {Server} from "./server.js";
 const App = new Server();
-
+console.log("=== CURRENT ENVIROMENT ===")
+console.log("NodeJS Version: ", process.version)
+console.log("==========================")
 App.listen();
